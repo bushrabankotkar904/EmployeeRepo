529f48b51a79d0ccaac8ca4d54a40f77834d9611
Bushra <bushrabankotkar@gmail.com>
2024-10-31 16:12:25 +0530

    Context file

---
+++ EmpMVC/Data/EmployeeDbContext.cs
@@ -0,0 +1,16 @@
+﻿using EmpMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmpMVC.Data
+{
+    public class EmployeeDbContext: DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
